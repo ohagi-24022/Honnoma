f4708703a10f601d08819d91d87f05f31475235d
ohagi-24022 <ep24022-3165@sti.chubu.ac.jp>
2026-09-16 16:46:11 +0900

    Fix plist import in iOS icon patch hook

--- scripts/patch-ios-icons-after-prebuild.js
+++ scripts/patch-ios-icons-after-prebuild.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const plist = require('@expo/plist');
+const plist = require('@expo/plist').default;
 
 const projectRoot = process.cwd();
 
